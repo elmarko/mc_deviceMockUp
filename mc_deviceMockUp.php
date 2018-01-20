9442a6a4e89316acaafded7ab7d201712a6faebe
elmarko <markcormack@gmail.com>
2018-01-20 01:03:41 +0000

    Fixes regex for 4s devices and allows videos to be wrapped in the short code. Adds a class to the mockup_cnt to distinguish if video or image. Various css fixes to accomodate video.

--- mc_deviceMockUp.php
+++ mc_deviceMockUp.php
@@ -3,7 +3,7 @@
    Plugin Name: MC Device Mock Up
    Plugin URI: http://markcormack.co.uk
    Description: a plugin that simply wraps enclosed images in a device mockup use shortcode [mc_deviceMockUp caption="" specificStyles=""]***IMGS IN HERE***[/mc_deviceMockUp]
-   Version: 0.1
+   Version: 0.2
    Author: Mark Cormack
    Author URI: http://markcormack.co.uk
    License: GPL2
@@ -62,16 +62,16 @@ function checkForVariancesIntThePassedModelString($modelString){
 		case (preg_match('/[iphone]?-?\d-?plus/i', $modelString) ? true : false):
 			return "iphone-6-plus";
 			break;
-		case (preg_match('/[iphone]?-?\d/i', $modelString) ? true : false):
+		case (preg_match('/[iphone]?-?\[6-8]/i', $modelString) ? true : false):
 			return "iphone-6";
 			break;
 		case (preg_match('/[iphone]?-?se/i', $modelString) ? true : false):
 			return "iphone-se";
 			break;
-		case (preg_match('/[iphone]?-?\5\D?/i', $modelString) ? true : false):
+		case (preg_match('/[iphone]?-?5[sc]?/i', $modelString) ? true : false):
 			return "iphone-se";
 			break;
-		case (preg_match('/[iphone]?-?4\D?/i', $modelString) ? true : false):
+		case (preg_match('/iPhone-?[3-4]s?/i', $modelString) ? true : false):
 			return "iphone-4s";
 			break;
 		case (preg_match('/[apple]?-?watch/i', $modelString) ? true : false):
@@ -103,19 +103,8 @@ function returnNumberOfImagesAsAString($numberOfImages){
     }
 }
 
-function mc_deviceMockUp_to_frontend_shortcode_func($atts = [], $content = null){ 
-	mc_deviceMockUp_add_my_css_and_my_js_files();
-	$specificStyles = $atts["styles"];
-	$caption = $atts["caption"];
-	$model = $atts["model"];
-	$models = $atts["models"];
-	$specificStylesToUse = returnSpecificStyles($specificStyles);
-	$enclosingElementToUse = returnEnclosingElement($caption);
-	$figCaptionToUse = returnFigCaption($caption);
-	$modelsToUse = returnModelsToUse($model, $models);
-	ob_start();
-		preg_match_all('/<img([^>]+)?>/', $content, $matches);
-		$sortedArray = [];
+function prepareAnyImages($matches, $modelsToUse){
+	$sortedArray = [];
 		$ix = 0;
 		$modelCount = count($modelsToUse);
 		foreach ($matches[1] as $match){
@@ -127,9 +116,49 @@ function mc_deviceMockUp_to_frontend_shortcode_func($atts = [], $content = null)
 			}
 		} 
 		$stringToReturn = implode($sortedArray);
+		return $stringToReturn;
+}
+
+function prepareAnyVideo($matches, $modelsToUse){
+	$sortedArray = [];
+		$ix = 0;
+		$modelCount = count($modelsToUse);
+		foreach ($matches[1] as $match){
+			$sortedArray[] = '<div class="mc_mockup '.$modelsToUse[$ix].'">'.$match.'</div>';
+			if (($ix + 1) < $modelCount){
+				$ix += 1;
+			}else{
+				$ix = 0;
+			}
+		} 
+		$stringToReturn = implode($sortedArray);
+		return $stringToReturn;
+}
+
+function mc_deviceMockUp_to_frontend_shortcode_func($atts = [], $content = null){ 
+	mc_deviceMockUp_add_my_css_and_my_js_files();
+	$specificStyles = $atts["styles"];
+	$caption = $atts["caption"];
+	$model = $atts["model"];
+	$models = $atts["models"];
+	$specificStylesToUse = returnSpecificStyles($specificStyles);
+	$enclosingElementToUse = returnEnclosingElement($caption);
+	$figCaptionToUse = returnFigCaption($caption);
+	$modelsToUse = returnModelsToUse($model, $models);
+	$typeClass = "";
+	//var_dump($modelsToUse);
+	ob_start();
+		$stringToReturn = "";
+		if (preg_match_all('/<img([^>]+)?>/', $content, $matches)){
+			$stringToReturn = prepareAnyImages($matches, $modelsToUse);
+			$typeClass = "image";
+		}else if (preg_match_all('/(<video\b[^>]*>.*?<\/video>)/', $content, $matches)){
+			$stringToReturn = prepareAnyVideo($matches, $modelsToUse);
+			$typeClass = "video";
+		}
 		$numberOfImages = count($matches[1]);
 		$imageCountString = returnNumberOfImagesAsAString($numberOfImages);
-		echo '<'.$enclosingElementToUse.' class="mc_mockup_cnt '.$imageCountString.'" '.$specificStylesToUse.' >';
+		echo '<'.$enclosingElementToUse.' class="mc_mockup_cnt '.$typeClass.' '.$imageCountString.'" '.$specificStylesToUse.' >';
 		echo $stringToReturn;
 		echo $figCaptionToUse;
 		echo '</'.$enclosingElementToUse.'>';	
